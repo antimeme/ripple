81de3392203dea12dbdf04f5aaa3ebe7d664b149
Jeff Gold <jgold@esclat.net>
2016-07-16 15:35:56 -0400

    onball: zooming and wireframe

--- apps/onball.js
+++ apps/onball.js
@@ -1,7 +1,6 @@
 (function(exports) {
     var sqrt2 = Math.sqrt(2);
     var sqrt6 = Math.sqrt(6);
-
     var tetrahedron = [[0, 0, 1], [0, 2 * sqrt2 / 3, -1 / 3],
                        [sqrt6 / 3, -sqrt2 / 3, -1 / 3],
                        [-sqrt6 / 3, -sqrt2 / 3, -1 / 3]];
@@ -62,73 +61,103 @@
         }
     };
 
+    var createSubject = function(steps) {
+        // Create a tetrahedron inscribed within a unit sphere
+        var vertices = tetrahedron.slice();
+        var faces = [[0, 2, 1], [0, 3, 2], [0, 1, 3], [1, 2, 3]];
+        var materials = [];
+        var step;
+        var subvert = function(v1, v2) {
+            var vertex = [v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]];
+            var norm = Math.sqrt(
+                vertex[0] * vertex[0] + vertex[1] * vertex[1] +
+                    vertex[2] * vertex[2]);
+            vertex[0] /= norm;
+            vertex[1] /= norm;
+            vertex[2] /= norm;
+            return vertex;
+        }
 
+        for (step = 0; step < steps; ++step) {
+            var newfaces = [];
+            faces.forEach(function(face) {
+                vertices.push(subvert(
+                    vertices[face[0]], vertices[face[1]]));
+                vertices.push(subvert(
+                    vertices[face[1]], vertices[face[2]]));
+                vertices.push(subvert(
+                    vertices[face[0]], vertices[face[2]]));
+
+                newfaces.push([face[0], vertices.length - 3,
+                               vertices.length - 1, face[3]]);
+                newfaces.push([face[1], vertices.length - 2,
+                               vertices.length - 3, face[3]]);
+                newfaces.push([face[2], vertices.length - 1,
+                               vertices.length - 2, face[3]]);
+                newfaces.push([vertices.length - 1,
+                               vertices.length - 3,
+                               vertices.length - 2, face[3]]);
+            });
+            faces = newfaces;
+        }
+
+        var geometry = new THREE.Geometry();
+        vertices.forEach(function(vertex) {
+            geometry.vertices.push(
+                new THREE.Vector3().fromArray(vertex));
+        });
+        faces.forEach(function(face, index) {
+            geometry.faces.push(
+                new THREE.Face3(face[0], face[1], face[2]));
+        });
+        return THREE.SceneUtils.createMultiMaterialObject(
+            geometry, [
+                new THREE.MeshBasicMaterial({ color: 0x8080ff }),
+                new THREE.MeshBasicMaterial(
+                    { color: 0x000000, wireframe: true,
+                      transparent: true })]);
+    }
+
+    // Creates a game rendered using three.js
     onball.go = function(parent, viewport) {
+        var minzoom = 1.1;
         var scene = new THREE.Scene();
         var camera = new THREE.PerspectiveCamera(
             75, viewport.width() / viewport.height(), 0.1, 1000);
-        var renderer = new THREE.WebGLRenderer();
+        var renderer = new THREE.WebGLRenderer({antialias: true});
+        renderer.setClearColor(0x202020);
         renderer.setSize(viewport.width(), viewport.height());
         parent.append(renderer.domElement);
 
-        var geometry = new THREE.Geometry();
-        geometry.vertices.push(
-            new THREE.Vector3().fromArray(tetrahedron[0]),
-            new THREE.Vector3().fromArray(tetrahedron[1]),
-            new THREE.Vector3().fromArray(tetrahedron[2]),
-            new THREE.Vector3().fromArray(tetrahedron[3]));
-        geometry.faces.push(
-            new THREE.Face3(0, 2, 1, undefined, undefined, 0),
-            new THREE.Face3(0, 3, 2, undefined, undefined, 1),
-            new THREE.Face3(0, 1, 3, undefined, undefined, 2),
-            new THREE.Face3(1, 2, 3, undefined, undefined, 3));
-
-        var material = new THREE.MeshFaceMaterial([
-            new THREE.MeshBasicMaterial({
-                color: 0x00ff00
-            }),
-            new THREE.MeshBasicMaterial({
-                color: 0xff0000
-            }),
-            new THREE.MeshBasicMaterial({
-                color: 0x0000ff,
-            }),
-            new THREE.MeshBasicMaterial({
-                color: 0xffff00
-            }),
-            new THREE.MeshBasicMaterial({
-                color: 0x00ffff
-            }),
-            new THREE.MeshBasicMaterial({
-                color: 0xff00ff
-            }),
-            new THREE.MeshBasicMaterial({
-                color: 0xffffff
-            }),
-            new THREE.MeshBasicMaterial({
-                color: 0x222222
-            })
-        ]);
-        /* */
-
-        var subject = new THREE.Mesh(geometry, material);
-        subject.rotation.x = Math.PI/4;
-        subject.rotation.y = Math.PI/4;
+        var subject = createSubject(Math.min(8, parseInt(
+            window.params['steps'], 10)));
+        subject.rotation.x = 0;
+        subject.rotation.y = Math.PI / 2;
         scene.add(subject);
 
+        var light = new THREE.DirectionalLight( 0xffffff );
+        light.position.set(1, 1, 1);
+        scene.add(light);
 
-        camera.position.z = 5;
+        camera.position.z = Math.max(minzoom, parseInt(
+            window.params['zoom'], 10) || 2);
 
-        /* */
         var isDragging = false;
         var previousMousePosition = {
             x: 0,
             y: 0
         };
-        $(renderer.domElement).on('mousedown', function(e) {
+
+        var self = $(renderer.domElement);
+        self.on('mousedown', function(e) {
             isDragging = true;
-        }).on('mousemove', function(e) {
-            //console.log(e);
+        });
+
+        self.on('mouseup mouseleave', function(e) {
+            isDragging = false;
+        });
+
+        self.on('mousemove', function(e) {
             var deltaMove = {
                 x: e.offsetX-previousMousePosition.x,
                 y: e.offsetY-previousMousePosition.y
@@ -153,19 +182,11 @@
             };
         });
 
-        $(document).on('mouseup', function(e) {
-            isDragging = false;
+        self.on('mousewheel', function(event) {
+            camera.position.z += 0.2 * event.deltaY;
+            if (camera.position.z < minzoom)
+                camera.position.z = minzoom;
         });
-
-        // shim layer with setTimeout fallback
-        window.requestAnimFrame = (function(){
-            return  window.requestAnimationFrame ||
-                window.webkitRequestAnimationFrame ||
-                window.mozRequestAnimationFrame ||
-                function(callback) {
-                    window.setTimeout(callback, 1000 / 60);
-                };
-        })();
 
         var lastFrameTime = new Date().getTime() / 1000;
         var totalGameTime = 0;
@@ -184,11 +205,8 @@
 
         function render() {
             renderer.render(scene, camera);
-            
-            
-            requestAnimFrame(render);
+            requestAnimationFrame(render);
         }
-
         render();
         update(0, totalGameTime);
 
