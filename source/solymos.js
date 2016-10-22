68b7e8cac6d68f264a60287247b6188e23d85db4
Jeff Gold <jgold@esclat.net>
2016-10-22 14:47:31 -0400

    solymos: improved error pages and negotiation

--- source/solymos.js
+++ source/solymos.js
@@ -1,5 +1,5 @@
 // Solymos
-// Copyright (C) 2013 by Jeff Gold.
+// Copyright (C) 2013-2016 by Jeff Gold.
 //
 // This program is free software: you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as
@@ -49,27 +49,38 @@ if (typeof require !== 'undefined') (function(solymos) {
     var url   = require('url');
     var querystring = require('querystring');
 
-    solymos.sanitizeURL = function(target, index) {
+    /**
+     * Process a URL from an untrusted source.  Consolidates path
+     * components and ensures that the path does not move above the
+     * current directory (which might allow accesss to unexpected
+     * files). */
+    solymos.sanitizeURL = function(target, index, dir) {
         var result = {};
-        var segments = [], ii, current;
+        var components = [], ii, current;
         var urlObj = url.parse(target, true);
-        var s = querystring.unescape(urlObj.pathname).split('/');
+        var s = querystring.unescape(
+            urlObj.pathname.replace(/\+/g, ' ')).split('/');
+
         for (ii = 0; ii < s.length; ++ii) {
             current = s[ii].trim();
             if (!current || current === '.')
                 continue;
             else if (current === '..')
-                segments.pop();
-            else segments.push(current);
+                components.pop();
+            else components.push(current);
         }
-        if (!segments.length && index)
-            segments.push(index);
-        result.path = segments.join('/');
-        segments.unshift('.');
-        result.fileName = segments.join(path.sep);
+
+        if (!components.length && index)
+            components.push(index);
+        result.path = components.join('/');
+        components.unshift(dir || '.');
+        result.fileName = components.join(path.sep);
         return result;
     };
 
+    /**
+     * Choses a file by adding an extension according to which MIME
+     * types are preferred. */
     var negotiateAccept = function(accept) {
         var ordered = [], unordered = {};
         var ii, entry, mtype, qvalue, quality = '1';
@@ -97,6 +108,7 @@ if (typeof require !== 'undefined') (function(solymos) {
         return {
             outstanding: 0, done: false,
             stages: ordered,
+            wildcard: false,
             start: function(server, fileName, response) {
                 var extensions = {};
                 var stage = this.stages.shift();
@@ -108,18 +120,42 @@ if (typeof require !== 'undefined') (function(solymos) {
                             extensions['html'] = true;
                         } else if (entry === 'application/xhtml+xml') {
                             extensions['xhtml'] = true;
+                        } else if (entry === 'application/xml') {
+                            extensions['xml'] = true;
+                        } else if (entry === 'text/css') {
+                            extensions['css'] = true;
                         } else if (entry === 'application/json') {
-                            extensions['js'] = true;
-                        } else if (entry === 'text/javascript') {
                             extensions['json'] = true;
+                        } else if (entry === 'text/javascript') {
+                            extensions['js'] = true;
+                        } else if (entry === 'image/jpeg') {
+                            extensions['jpg'] = true;
+                            extensions['jpeg'] = true;
+                        } else if (entry === 'image/gif') {
+                            extensions['gif'] = true;
+                        } else if (entry === 'image/png') {
+                            extensions['png'] = true;
+                        } else if (entry === 'image/*') {
+                            extensions['jpg'] = true;
+                            extensions['jpeg'] = true;
+                            extensions['gif'] = true;
+                            extensions['png'] = true;
                         } else if (entry === '*/*') {
-                            extensions['xhtml'] = true;
+                            self.wildcard = true;
                             extensions['html']  = true;
-                            extensions['js']    = true;
+                            extensions['xhtml'] = true;
+                            extensions['xml']    = true;
+                            extensions['css']   = true;
                             extensions['json']  = true;
+                            extensions['js']    = true;
+                            extensions['jpg']   = true;
+                            extensions['jpeg']  = true;
+                            extensions['gif']   = true;
+                            extensions['png']   = true;
                         } else console.log("UNCERTAIN:", entry);
                     });
-                } else return server.errorPage(response, 404, fileName);
+                } else return server.errorPage(
+                    response, this.wildcard ? 404 : 406, fileName);
 
                 if (Object.keys(extensions).length === 0)
                     return server.errorPage(response, 500, fileName);
@@ -151,7 +187,7 @@ if (typeof require !== 'undefined') (function(solymos) {
 
     var handleRequest = function(server, request, response) {
         var key, service = null, target = solymos.sanitizeURL(
-            request.url, server.defaultPage);
+            request.url, server.defaultPage, server.directory);
         console.log(new Date().toISOString(),
                     'INFO: request', target.path);
 
@@ -161,11 +197,16 @@ if (typeof require !== 'undefined') (function(solymos) {
                 if (matchService(target.path, key))
                     service = server.services[target.path];
             });
-        if (!service && server.defaultService &&
-            matchService(target.path, path.basename(process.argv[1])))
+        if (!service && server.defaultService && matchService(
+            target.path, path.basename(process.argv[1])))
             service = server.defaultService;
         if (service)
-            return service(server, request, response, target.fileName);
+            return service.call(server, target.fileName,
+                                request, response);
+
+        // Serving files can be disabled using false or null
+        if (server.directory === false || server.directory === null)
+            return server.errorPage(response, 404, target.fileName);
 
         // Otherwise unrecognized URLs are treated as file paths
         fs.readFile(target.fileName, function (err, data) {
@@ -189,6 +230,12 @@ if (typeof require !== 'undefined') (function(solymos) {
                        options.portHTTP : 80),
             portHTTPS: ((options && options.portHTTPS) ?
                         options.portHTTPS : 443),
+            certificate: ((options && options.certificate) ?
+                          options.certificate : 'certificate.pem'),
+            privteKey: ((options && options.privateKey) ?
+                        options.privateKey : 'private-key.pem'),
+            directory: ((options && options.directory) ?
+                        options.directory : undefined),
             defaultService: options && options.defaultService,
             services: ((options && options.services) ?
                        options.services : {}),
@@ -205,7 +252,7 @@ if (typeof require !== 'undefined') (function(solymos) {
                           null : 'HTTP'));
 
                 if (iface === 'CGI') {
-                    console.log('CGI'); // FIXME
+                    console.log('CGI'); // FIXME: implement!
                 } else if (iface === 'HTTP') {
                     http.createServer(function(request, response) {
                         handleRequest(server, request, response);
@@ -216,9 +263,9 @@ if (typeof require !== 'undefined') (function(solymos) {
                 } else if (iface === 'HTTPS') {
                     const https = require('https');
                     httpsOptions = {
-                        key: fs.readFileSync('grimoire-key.pem'),
-                        cert: fs.readFileSync('grimoire-cert.pem')
-                    }; // FIXME
+                        key: fs.readFileSync(this.privateKey),
+                        cert: fs.readFileSync(this.certificate)
+                    }; // FIXME: support automatic certificates?
 
                     https.createServer(httpsOptions, function(
                         request, response) {
@@ -242,6 +289,7 @@ if (typeof require !== 'undefined') (function(solymos) {
                         'html':  'text/html',
                         'css':   'text/css',
                         'png':   'image/png',
+                        'gif':   'image/gif',
                         'jpeg':  'image/jpeg',
                         'jpg':   'image/jpeg',
                         'js':    'text/javascript',
@@ -265,36 +313,45 @@ if (typeof require !== 'undefined') (function(solymos) {
                     this.errorPage(response, 404, fileName);
                 else if (err.code === 'EACCES')
                     this.errorPage(response, 403, fileName);
-                else this.errorPage(response, 500, fileName);
+                else this.errorPage(response, 500, fileName, err.code);
             },
 
-            errorPage: function(response, code, fileName) {
+            errorPage: function(response, code, fileName, message) {
+                var server = this;
+                var explainError = function(err) {
+                    var errtable = {
+                        400: 'Bad Request',
+                        401: 'Unauthorized',
+                        403: 'Forbidden',
+                        404: 'Not Found',
+                        406: 'Not Acceptable',
+                        500: 'Internal Server Error',
+                    };
+                    var description = (
+                        errtable[code] || 'No Description Available');
+                    var data = [
+                        '<!doctype html>', '<meta charset="utf-8">',
+                        '<h1>HTTP Error ' + code + ': ' +
+                        description + '</h1>',
+                        (message ? '<p>' + message + '</p>' : ''),
+                        '<p>Attempt to access error page got ' +
+                        err.code + '</p>',
+                        '<hr />', '<p>' + server.serverName + '</p>'];
+                    response.setHeader('Content-Type', 'text/html');
+                    response.writeHeader(code);
+                    response.end(data.join('\r\n'));
+                    console.log(new Date().toISOString(),
+                                'ERROR:', code, fileName,
+                                '(errpage: ' + err.code + ')');
+                }
+
                 fs.readFile(
                     'errorpages/page' + code + '.html',
                     function(err, data) {
-                        if (err) {
-                            if (err.code === 'ENOENT') {
-                                // TODO: error explanation strings
-                                response.setHeader(
-                                    'Content-Type', 'text/html');
-                                response.writeHeader(code);
-                                response.end(
-                                    '<h1>HTTP ERROR ' + code + '</h1>');
-                                console.log(
-                                    new Date().toISOString(),
-                                    'ERROR:', code, fileName, '(NOPAGE)');
-                            } else {
-                                response.setHeader(
-                                    'Content-Type', 'text/html');
-                                response.writeHeader(500);
-                                response.end(err.toString());
-                                console.log(
-                                    new Date().toISOString(),
-                                    'ERROR: 500', fileName, err.code);
-                            }
-                            return;
-                        }
-                        response.setHeader('Content-Type', 'text/html');
+                        if (err)
+                            return explainError(err);
+                        response.setHeader(
+                            'Content-Type', 'text/html');
                         response.writeHeader(code);
                         response.end(data.toString().replace(
                             /:PATH:/g, fileName));
