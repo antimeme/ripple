c5b79023eaf75e3787ed7d2c7a1ad621ffd0c1c5
Jeff Gold <jgold@esclat.net>
2020-07-01 14:23:52 -0400

    RippleServlet: display client certificate information

--- source/servlet/RippleServlet.java
+++ source/servlet/RippleServlet.java
@@ -2,7 +2,8 @@ package net.esclat.ripple.servlet;
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.IOException;
-import javax.security.cert.X509Certificate;
+import java.security.cert.X509Certificate;
+import javax.security.auth.x500.X500Principal;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -42,9 +43,6 @@ public class RippleServlet extends HttpServlet
         out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
 
         if (request.isSecure()) {
-            X509Certificate certs[] = (X509Certificate[])
-                request.getAttribute
-                ("javax.servlet.request.X509Certificate");
             out.println("<p>Secure</p>");
             out.println("<p>SessionID: " + request.getAttribute
                 ("javax.servlet.request.ssl_session_id") + "</p>");
@@ -52,7 +50,26 @@ public class RippleServlet extends HttpServlet
                 ("javax.servlet.request.cipher_suite") + "</p>");
             out.println("<p>KeySize: " + request.getAttribute
                         ("javax.servlet.request.key_size") + "</p>");
+
+            X509Certificate certs[] = (X509Certificate[])
+                request.getAttribute
+                ("javax.servlet.request.X509Certificate");
+            if (certs != null) {
+                if (certs.length > 0) {
+                    out.println("<ul>");
+                    for (int ii = 0; ii < certs.length; ++ii) {
+                        X509Certificate cert = certs[ii];
+                        X500Principal subject =
+                            cert.getSubjectX500Principal();
+                        X500Principal issuer =
+                            cert.getIssuerX500Principal();
+                        out.println
+                            ("<li>Subject: " + subject.getName() +
+                             " (" + issuer.getName() + ")</li>");
+                    }
+                    out.println("</ul>");
+                }
+            } else out.println("<p>No client certificate</p>");
         } else { out.println("<p>Insecure</p>"); }
     }
-                              
 }
