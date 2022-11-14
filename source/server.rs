26d8fdbc8f0133f490f696f1558f2be63a9ebb55
Jeff Gold <jgold@esclat.net>
2022-11-13 21:21:27 -0500

    server.rs: progress on JSON example

--- source/server.rs
+++ source/server.rs
@@ -4,36 +4,82 @@ use rocket::{Rocket, Build, Request, Data};
 use rocket::fs::{relative, NamedFile};
 use rocket::serde::{Deserialize, json::Json};
 use rocket::route::{Route, Handler, Outcome};
-use rocket::http::Method;
+use rocket::http::{Method, ContentType};
 use rocket::http::uri::Segments;
 use rocket::http::ext::IntoOwned;
 use rocket::response::Redirect;
 
-// #[derive(Deserialize)]
-// #[serde(crate = "rocket::serde")]
-// struct Expense<'r> {
-//     name: &'r str,
-//     amount: f32,
-//     tags: Vec<&'r str>
-// }
-
-#[derive(Deserialize)]
+#[derive(Clone, Deserialize)]
 #[serde(crate = "rocket::serde")]
-struct Task<'r> {
-    description: &'r str,
-    complete: bool
+struct Expense<'r> {
+    name: &'r str,
+    amount: f32,
+    tags: Vec<&'r str>
 }
 
-#[post("/", data = "<task>")]
-fn todo(task: Json<Task<'_>>) -> String {
-    format!("Task (complete = {}): {}", task.complete, task.description)
+//static expenses: Vec<Expense> = Vec::new();
+
+#[get("/")]
+fn expense_index() -> (ContentType, String) {
+    (ContentType::HTML, String::from(r#"<!DOCTYPE html>
+<meta charset=\"utf-8\" />
+<style>
+  input:invalid { border: red solid 3px; }
+</style>
+<title>Expenses</title>
+<h1>Expenses</h1>
+<fieldset>
+  <legend>Add</legend>
+  <form id="addForm" method="POST" action="/add">
+    <input id="addName" type="text" placeholder="Purpose"/>
+    <input id="addAmount" type="text"
+           placeholder="$10.00" pattern="\$?[0-9]*.?[0-9]*"
+           title="Monetary amount with optional dollar sign"/>
+    <span class="validity"></span>
+    <input id="addTags" type="text" placeholder="food groceries"/>
+    <button id="addSubmit" type="submit">Submit</button>
+    <button id="addClear" type="button">Clear</button>
+  </form>
+</fieldset>
+<textarea id="response" disabled></textarea>
+<script>
+    var addForm   = document.getElementById("addForm");
+    var addClear  = document.getElementById("addClear");
+    var addSubmit = document.getElementById("addSubmit");
+    var addName   = document.getElementById("addName");
+    var addAmount = document.getElementById("addAmount");
+    var addTags   = document.getElementById("addTags");
+    addClear.addEventListener('click', function(event) {
+        addName.value = "";
+        addAmount.value = "$0.00";
+        addTags.value = "";
+        event.preventDefault();
+    });
+    addForm.addEventListener('submit', function(event) {
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener('load', function() {
+            response.value = this.responseText;
+        })
+        // TODO: make this work with /expense as well as /expense/
+        xhr.open('POST', "./add");
+        xhr.setRequestHeader('content-type', 'application/json');
+        xhr.send(JSON.stringify({
+            name: addName.value,
+            amount: parseFloat(addAmount.value.replace("$", "")),
+            tags: addTags.value.split(/\s/).filter(function(w) {
+                return w.trim().length > 0; })}));
+        response.value = "Waiting for response...";
+        event.preventDefault();
+    })
+</script>"#))
 }
 
-#[get("/<name>/<age>")]
-fn greeting(name: String, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
+#[post("/add", data="<expense>")]
+fn expense_add(expense: Json<Expense<'_>>) -> (ContentType, String) {
+    // TODO: add expense to expenses
+    //expenses.push(expense.into_inner());
+    (ContentType::JSON, String::from("{\"key\": \"value\"}"))
 }
-
 
 /**
  * A modified version of Rocket FileServer that accepts root URIs and
@@ -129,8 +175,7 @@ fn create_server() -> Rocket<Build> {
             relative!("resources/images/ripple.png")).rank(1))
         .mount("/apps", FileExtServer::new(relative!("apps")))
         .mount("/slides", FileExtServer::new(relative!("slides")))
-        .mount("/greeting", routes![greeting])
-        .mount("/todo", routes![todo])
+        .mount("/expense", routes![expense_index, expense_add])
 }
 
 #[rocket::main]
