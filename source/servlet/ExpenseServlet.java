e121bedb52b83548939f2efbeaa71f7a0bf4a3ba
Jeff Gold <jgold@esclat.net>
2023-02-25 00:50:55 -0500

    ExpenseServlet: progress on SQL

--- source/servlet/ExpenseServlet.java
+++ source/servlet/ExpenseServlet.java
@@ -15,72 +15,82 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-@WebServlet("/")
+@WebServlet("/expense")
 public class ExpenseServlet extends HttpServlet {
+    Connection connection = null;
+
     @Override
-    protected void doGet(HttpServletRequest req,
-                         HttpServletResponse resp)
-        throws ServletException, IOException {
+    public void init() throws ServletException {
+        String dbPath = getServletContext().getInitParameter("dbPath");
+        String jdbcUrl = "jdbc:sqlite:" + dbPath;
         try {
             Class.forName("org.sqlite.JDBC");
-        } catch (Exception e) {
-            resp.getWriter().write(e.getMessage());
-            return;
-        }
+            connection = DriverManager.getConnection(jdbcUrl);
 
-        Connection connection = null;
-        try {
-            // create a database connection
-            connection = DriverManager.getConnection
-                (String.format("jdbc:sqlite:%s",
-                               getServletContext()
-                               .getRealPath("WEB-INF/sample.db")));
+            // TODO: create tables
             Statement statement = connection.createStatement();
             statement.setQueryTimeout(30);  // set timeout to 30 sec.
-            statement.executeUpdate("drop table if exists person");
             statement.executeUpdate
-                ("create table person (id integer, name string)");
-
+                ("CREATE TABLE IF NOT EXISTS expenses (" +
+                 "  id INTEGER PRIMARY KEY," +
+                 "  reason TEXT NOT NULL," +
+                 "  amount REAL NOT NULL," +
+                 "  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);");
             connection.setAutoCommit(false);
 
             try {
                 PreparedStatement insertStatement =
                     connection.prepareStatement
-                    ("insert into person values(?, ?)");
-                insertStatement.setInt(1, 1);
-                insertStatement.setString(2, "leo");
+                    ("INSERT INTO expenses (reason, amount) " +
+                     "VALUES(?, ?)");
+                insertStatement.setString(1, "Soapy Sponges");
+                insertStatement.setFloat(2, 5.99f);
                 insertStatement.executeUpdate();
-                insertStatement.setInt(1, 2);
-                insertStatement.setString(2, "yui");
+                insertStatement.setString(1, "Silky Sponges");
+                insertStatement.setFloat(2, 6.99f);
                 insertStatement.executeUpdate();
                 connection.commit();
-            } catch (SQLException e) {
-                resp.getWriter().write(e.getMessage());
+            } catch (SQLException ex) {
                 connection.rollback();
+                throw new ServletException
+                    ("Failed to initialize database", ex);
             }
 
             connection.setAutoCommit(true);
+        } catch (SQLException | ClassNotFoundException ex) {
+            throw new ServletException("Failed to create database", ex);
+        }
+    }
 
-            ResultSet rs = statement.executeQuery("select * from person");
+    @Override
+    public void destroy() {
+        try {
+            if (connection != null)
+                connection.close();
+        } catch (SQLException ex) { /* TODO: log this */ }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+        throws ServletException, IOException {
+        try {
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+
+            ResultSet rs = statement.executeQuery
+                ("SELECT reason, amount FROM expenses;");
             while (rs.next()) {
                 // read the result set
-                resp.getWriter().write("name = " + rs.getString("name"));
-                resp.getWriter().write(", ");
-                resp.getWriter().write("id = " + rs.getInt("id"));
-                resp.getWriter().write("\n");
+                response.getWriter().write
+                    ("reason = " + rs.getString("name"));
+                response.getWriter().write(", ");
+                response.getWriter().write
+                    ("amount = " + rs.getFloat("id"));
+                response.getWriter().write("\n");
             }
-        } catch (SQLException e) {
-            // if the error message is "out of memory",
-            // it probably means no database file is found
-            resp.getWriter().write(e.getMessage());
-        } finally {
-            try {
-                if (connection != null)
-                    connection.close();
-            } catch (SQLException e) {
-                // connection close failed.
-                resp.getWriter().write(e.getMessage());
-            }
+        } catch (SQLException ex) {
+            response.getWriter().write(ex.getMessage());
         }
     }
 }
