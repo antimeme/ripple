e512c1ed6cfe4509965dcd0640c75393321ce7e9
Jeff Gold <jgold@esclat.net>
2023-02-27 22:16:07 -0500

    Reworked noise.js as JavaScript module

--- source/noise.mjs
+++ source/noise.mjs
@@ -1,5 +1,5 @@
 // noise.js
-// Copyright (C) 2022 by Jeff Gold.
+// Copyright (C) 2022-2023 by Jeff Gold.
 //
 // This program is free software: you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as
@@ -16,115 +16,175 @@
 // <http://www.gnu.org/licenses/>.
 //
 // ---------------------------------------------------------------------
-(function(noise) {
-    'use strict';
+//
 
-    noise.SquirrelNoise5 = function(seed, px, py, pz, pt) {
-        // Squirrel Eiserloh's raw noise function
-        // https://twitter.com/SquirrelTweets/status/1421251894274625536
-        //
-        // This code is made available under the Creative Commons
-        // attribution 3.0 license (CC-BY-3.0 US):
-        // Attribution in source code comments (even
-        // closed-source/commercial code) is sufficient.
-        // License summary and text available at:
-        // https://creativecommons.org/licenses/by/3.0/us/
-        if (isNaN(seed))
-            throw new Error("Seed must be numeric: " + seed);
-        if (isNaN(px))
-            throw new Error("Position x must be numeric: " + px);
+var noiseSquirrel5 = function(seed, px, py, pz, pt) {
+    // Squirrel Eiserloh's raw noise function
+    // https://twitter.com/SquirrelTweets/status/1421251894274625536
+    //
+    // This code is made available under the Creative Commons
+    // attribution 3.0 license (CC-BY-3.0 US):
+    // Attribution in source code comments (even
+    // closed-source/commercial code) is sufficient.
+    // License summary and text available at:
+    // https://creativecommons.org/licenses/by/3.0/us/
+    if (isNaN(seed))
+        throw new Error("Seed must be numeric: " + seed);
+    if (isNaN(px))
+        throw new Error("Position x must be numeric: " + px);
 
-	var mangledBits = Math.floor(px);
-        if (!isNaN(py))
-            mangledBits += 198491317 * Math.floor(py);
-        if (!isNaN(pz))
-            mangledBits += 6542989 * Math.floor(pz);
-        if (!isNaN(pt))
-            mangledBits += 357239 * Math.floor(pt);
+    var mangledBits = Math.floor(px);
+    if (!isNaN(py))
+        mangledBits += 198491317 * Math.floor(py);
+    if (!isNaN(pz))
+        mangledBits += 6542989 * Math.floor(pz);
+    if (!isNaN(pt))
+        mangledBits += 357239 * Math.floor(pt);
 
-	const SQ5_BIT_NOISE1 = 0xd2a80a3f;
-	// 11010010101010000000101000111111
-	const SQ5_BIT_NOISE2 = 0xa884f197;
-	// 10101000100001001111000110010111
-	const SQ5_BIT_NOISE3 = 0x6C736F4B;
-        // 01101100011100110110111101001011
-	const SQ5_BIT_NOISE4 = 0xB79F3ABB;
-	// 10110111100111110011101010111011
-	const SQ5_BIT_NOISE5 = 0x1b56c4f5;
-	// 00011011010101101100010011110101
-        mangledBits *= SQ5_BIT_NOISE1;
-	mangledBits += Math.floor(seed);
-	mangledBits ^= (mangledBits >> 9);
-	mangledBits += SQ5_BIT_NOISE2;
-	mangledBits ^= (mangledBits >> 11);
-	mangledBits *= SQ5_BIT_NOISE3;
-	mangledBits ^= (mangledBits >> 13);
-	mangledBits += SQ5_BIT_NOISE4;
-	mangledBits ^= (mangledBits >> 15);
-	mangledBits *= SQ5_BIT_NOISE5;
-	mangledBits ^= (mangledBits >> 17);
-	return mangledBits;
+    const SQ5_BIT_NOISE1 = 0xd2a80a3f;
+    // 11010010101010000000101000111111
+    const SQ5_BIT_NOISE2 = 0xa884f197;
+    // 10101000100001001111000110010111
+    const SQ5_BIT_NOISE3 = 0x6C736F4B;
+    // 01101100011100110110111101001011
+    const SQ5_BIT_NOISE4 = 0xB79F3ABB;
+    // 10110111100111110011101010111011
+    const SQ5_BIT_NOISE5 = 0x1b56c4f5;
+    // 00011011010101101100010011110101
+    mangledBits *= SQ5_BIT_NOISE1;
+    mangledBits += Math.floor(seed);
+    mangledBits ^= (mangledBits >> 9);
+    mangledBits += SQ5_BIT_NOISE2;
+    mangledBits ^= (mangledBits >> 11);
+    mangledBits *= SQ5_BIT_NOISE3;
+    mangledBits ^= (mangledBits >> 13);
+    mangledBits += SQ5_BIT_NOISE4;
+    mangledBits ^= (mangledBits >> 15);
+    mangledBits *= SQ5_BIT_NOISE5;
+    mangledBits ^= (mangledBits >> 17);
+    return mangledBits;
+};
+
+var perlinSimplex3 = function(x, y, z) {
+    // Adapted from chapter 2 of Ken Perlin's 2001 SIGGRAPH
+    // Course Notes.
+    var A = [];
+    var T = [0x15,0x38,0x32,0x2c,0x0d,0x13,0x07,0x2a];
+    var bi = function(N, B) { return N>>B & 1; };
+    var b = function(i, j, k, B) {
+        return T[bi(i,B)<<2 | bi(j,B)<<1 | bi(k,B)];
     };
+    var shuffle = function(i, j, k) {
+        return b(i,j,k,0) + b(j,k,i,1) + b(k,i,j,2) + b(i,j,k,3) +
+            b(j,k,i,4) + b(k,i,j,5) + b(i,j,k,6) + b(j,k,i,7) ;
+    }
+
+    var K = function(a) {
+        var s = (A[0]+A[1]+A[2])/6.;
+        var x = u - A[0] + s,
+            y = v - A[1] + s,
+            z = w - A[2] + s,
+            t = .6 - x * x - y * y - z * z;
+        var h = shuffle(i + A[0], j + A[1], k + A[2]);
+        A[a]++;
+        if (t < 0)
+            return 0;
+        var b5 = h>>5 & 1,
+            b4 = h>>4 & 1,
+            b3 = h>>3 & 1,
+            b2 = h>>2 & 1,
+            b = h & 3;
+        var p = b==1 ? x : b==2 ? y : z,
+            q = b==1 ? y : b==2 ? z : x,
+            r = b==1 ? z : b==2 ? x : y;
+        p = (b5==b3 ? -p : p);
+        q = (b5==b4 ? -q : q);
+        r = (b5!=(b4^b3) ? -r : r);
+        t *= t;
+        return 8 * t * t * (p + (b==0 ? q+r : b2==0 ? q : r));
+    }
+
+    if (isNaN(x))
+        x = 0;
+    if (isNaN(y))
+        y = 0;
+    if (isNaN(z))
+        z = 0;
+    var i, j, k;
+    var u, v, w;
+    var s = (x + y + z)/3;
+
+    i = Math.floor(x + s);
+    j = Math.floor(y + s);
+    k = Math.floor(z + s);
+    s = (i + j + k)/6.;
+    u = x - i + s;
+    v = y - j + s;
+    w = z - k + s;
+
+    A[0]=A[1]=A[2]=0;
+    var hi = u>=w ? u>=v ? 0 : 1 : v>=w ? 1 : 2;
+    var lo = u< w ? u< v ? 0 : 1 : v< w ? 1 : 2;
     
-    noise.perlinSimplex3 = function(x, y, z) {
-        // Adapted from chapter 2 of Ken Perlin's 2001 SIGGRAPH
-        // Course Notes.  This is used as a referene to compare
-        // against the less inscrutible version above.
-        var A = [];
-        var T = [0x15,0x38,0x32,0x2c,0x0d,0x13,0x07,0x2a];
-        var bi = function(N, B) { return N>>B & 1; };
-        var b = function(i, j, k, B) {
-            return T[bi(i,B)<<2 | bi(j,B)<<1 | bi(k,B)];
-        };
-        var shuffle = function(i, j, k) {
-            return b(i,j,k,0) + b(j,k,i,1) + b(k,i,j,2) + b(i,j,k,3) +
-                   b(j,k,i,4) + b(k,i,j,5) + b(i,j,k,6) + b(j,k,i,7) ;
+    return K(hi) + K(3-hi-lo) + K(lo) + K(0);
+};
+
+export default function(config) {
+    var result = undefined;
+
+    // Choose a noise algorithm
+    var algorithm = (config && config.algorithm) ?
+        config.algorithm : undefined;
+    var seed = (config && config.seed) ? config.seed : 0;
+    if (algorithm === "squirrel") {
+        result = function(x, y) {
+            var seed = 100;
+            var value = noiseSquirrel5(seed, x, y);
+            return (value / (2 * (1 << 30)));
         }
-
-        var K = function(a) {
-            var s = (A[0]+A[1]+A[2])/6.;
-            var x = u - A[0] + s,
-                y = v - A[1] + s,
-                z = w - A[2] + s,
-                t = .6 - x * x - y * y - z * z;
-            var h = shuffle(i + A[0], j + A[1], k + A[2]);
-            A[a]++;
-            if (t < 0)
-                return 0;
-            var b5 = h>>5 & 1,
-                b4 = h>>4 & 1,
-                b3 = h>>3 & 1,
-                b2 = h>>2 & 1,
-                b = h & 3;
-            var p = b==1 ? x : b==2 ? y : z,
-                q = b==1 ? y : b==2 ? z : x,
-                r = b==1 ? z : b==2 ? x : y;
-            p = (b5==b3 ? -p : p);
-            q = (b5==b4 ? -q : q);
-            r = (b5!=(b4^b3) ? -r : r);
-            t *= t;
-            return 8 * t * t * (p + (b==0 ? q+r : b2==0 ? q : r));
+    } else if (algorithm === "perlin") {
+        result = function(x, y) {
+            var value = perlinSimplex3(x, y);
+            return (1 + (value / 0.32549)) / 2;
         }
+    } else {
+        result = noiseSimplex;
+    }
 
-        var i, j, k;
-        var u, v, w;
-        var s = (x + y + z)/3;
+    // Apply selected noise algorithm to a given canvas
+    var max = undefined, min = undefined, count = 6;
+    var canvas = (config && config.canvas) ? config.canvas : undefined;
+    if (result && canvas && canvas instanceof HTMLCanvasElement) {
+        const width = canvas.width;
+        const height = canvas.height;
+        const freq = (config && config.freq) ? config.freq : 0.05;
+        const data = new Uint8ClampedArray(4 * width * height);
 
-        i = Math.floor(x + s);
-        j = Math.floor(y + s);
-        k = Math.floor(z + s);
-        s = (i + j + k)/6.;
-        u = x - i + s;
-        v = y - j + s;
-        w = z - k + s;
+        for (var yy = 0; yy < height; ++yy)
+            for (var xx = 0; xx < width; ++xx) {
+                var index = 4 * (yy * width + xx);
+                var value = result(xx * freq, yy * freq);
+                if (isNaN(min) || (value < min))
+                    min = value;
+                if (isNaN(max) || (value > max))
+                    max = value;
+                value = Math.floor(255.99 * value);
+                value = Math.min(255, Math.max(0, value));
 
-        A[0]=A[1]=A[2]=0;
-        var hi = u>=w ? u>=v ? 0 : 1 : v>=w ? 1 : 2;
-        var lo = u< w ? u< v ? 0 : 1 : v< w ? 1 : 2;
-        return K(hi) + K(3-hi-lo) + K(lo) + K(0);
-    };
+                data[index + 0] = value;
+                data[index + 1] = value;
+                data[index + 2] = value;
+                data[index + 3] = 255;
+            }
 
-    noise.simplex = function() {
+        const ctx = canvas.getContext("2d");
+        ctx.putImageData(new ImageData(data, width, height), 0, 0);
+    }
+    //console.log("DEBUG-range", min, max);
+    return result;
+};
+
+var noiseSimplex = function() {
         // An implementation of Ken Perlin's Simplex Noise.
         //   https://en.wikipedia.org/wiki/Simplex_noise
         // Call with coordinates of point in order.  For example,
@@ -309,88 +369,3 @@
         }
         return result;
     };
-
-})(typeof exports === 'undefined' ? window['noise'] = {} : exports);
-
-if ((typeof require !== 'undefined') && (require.main === module)) {
-    var noise = exports;
-    var createGrayNoisePPM = function(fn, width, height) {
-        var ii, jj, min, max, now, last = Date.now();
-        console.log("P3");
-        console.log(width, " ", height);
-        console.log("255");
-        for (var jj = height; jj >= 0; --jj) {
-            now = Date.now();
-            if ((jj === height) || (now - last > 200 )) {
-                console.error("Scanlines remaining:", jj);
-                last = now;
-            }
-            for (var ii = 0; ii < width; ++ii) {
-                var value = fn(ii, jj)
-                if (isNaN(min) || (value < min))
-                    min = value;
-                if (isNaN(max) || (value > max))
-                    max = value;
-                value = Math.min(1, Math.max(0, value));
-                console.log(Math.floor(255.999 * value),
-                            Math.floor(255.999 * value),
-                            Math.floor(255.999 * value));
-            }
-        }
-        console.error("Done: min=" + min.toFixed(3) +
-                      " max=" + max.toFixed(3) +
-                      " diff=" + (max - min).toFixed(3));
-    };
-
-    var allowOptions = true;
-    var actions = [];
-    var freq = 0.01;
-    var width = 640;
-    var height = 480;
-    var noisefn = function(x, y) {
-        return (noise.simplex(
-            x * freq, y * freq) * 2 / 2.946 + 0.915) / 2;
-    };
-    var noisefns = {
-        perlin: function(x, y) {
-            return (noise.perlinSimplex3(
-                x * freq, y * freq, 1) * (2/0.58) + 1) / 2;
-        }
-    };
-    process.argv.slice(2).forEach(function(argument) {
-        if (allowOptions && (argument === "--")) {
-            allowOptions = false;
-        } else if (allowOptions && argument.startsWith("--")) {
-            if (argument.startsWith("--freq=")) {
-                freq = parseFloat(argument.slice("--freq=".length), 10);
-                if (isNaN(freq) || (freq < 0))
-                    throw new Error("Invalid width: " +
-                                    argument.slice("--width=".length));
-            } else if (argument.startsWith("--width=")) {
-                width = parseInt(argument.slice("--width=".length), 10);
-                if (isNaN(width) || (width < 0))
-                    throw new Error("Invalid width: " +
-                                    argument.slice("--width=".length));
-            } else if (argument.startsWith("--height=")) {
-                height = parseInt(
-                    argument.slice("--height=".length), 10);
-                if (isNaN(height) || (height < 0))
-                    throw new Error("Invalid height: " +
-                                    argument.slice("--height=".length));
-            } else if (argument === "--perlin")
-                noisefn = noisefns["perlin"];
-            else if (argument.startsWith("--noise=")) {
-                var fn_name = argument.slice("--noise=".length);
-                if (fn_name in noisefns)
-                    noisefn = noisefns[fn_name];
-                else throw new Error(
-                    "Unknown noise function: " + fn_name);
-            } else throw new Error(
-                "Unknown option: " + argument);
-        } else actions.push(argument);
-    });
-
-    //createGrayNoisePPM(noisefn, width, height);
-    for (var ii = 0; ii < 5; ++ii)
-        console.error("ii=" + ii, noise.SquirrelNoise5(0, ii));
-}
