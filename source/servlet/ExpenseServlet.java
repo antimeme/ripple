0d1659730c438e926cbe63b9120fcc441dd32d74
Jeff Gold <jgold@esclat.net>
2023-06-20 20:05:00 -0400

    ExpenseServlet.java: improved querys

--- source/servlet/ExpenseServlet.java
+++ source/servlet/ExpenseServlet.java
@@ -1,96 +1,97 @@
 package net.esclat.ripple.expense;
-// Inspired by
-// https://github.com/hadisfr/TomcatSQLite/blob/master/src/main/java/Srvlt.java
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 @WebServlet("/expense")
 public class ExpenseServlet extends HttpServlet {
-    Connection connection = null;
+    private static final long serialVersionUID = 1L;
+    private static final String SETUP_SQL =
+        "CREATE TABLE IF NOT EXISTS expenses (" +
+        "  id INTEGER PRIMARY KEY," +
+        "  reason TEXT NOT NULL," +
+        "  amount REAL NOT NULL," +
+        "  when TIMESTAMP DEFAULT CURRENT_TIMESTAMP);";
+
+    protected Connection conn = null;
 
     @Override
     public void init() throws ServletException {
+        super.init();
+
         String dbPath = getServletContext().getInitParameter("dbPath");
-        String jdbcUrl = "jdbc:sqlite:" + dbPath;
+        if (dbPath == null)
+            dbPath = "expense.db";
+
         try {
-            Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection(jdbcUrl);
+            conn = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
 
-            // TODO: create tables
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);  // set timeout to 30 sec.
-            statement.executeUpdate
-                ("CREATE TABLE IF NOT EXISTS expenses (" +
-                 "  id INTEGER PRIMARY KEY," +
-                 "  reason TEXT NOT NULL," +
-                 "  amount REAL NOT NULL," +
-                 "  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);");
-            connection.setAutoCommit(false);
-
-            try {
-                PreparedStatement insertStatement =
-                    connection.prepareStatement
-                    ("INSERT INTO expenses (reason, amount) " +
-                     "VALUES(?, ?)");
-                insertStatement.setString(1, "Soapy Sponges");
-                insertStatement.setFloat(2, 5.99f);
-                insertStatement.executeUpdate();
-                insertStatement.setString(1, "Silky Sponges");
-                insertStatement.setFloat(2, 6.99f);
-                insertStatement.executeUpdate();
-                connection.commit();
-            } catch (SQLException ex) {
-                connection.rollback();
-                throw new ServletException
-                    ("Failed to initialize database", ex);
-            }
-
-            connection.setAutoCommit(true);
-        } catch (SQLException | ClassNotFoundException ex) {
-            throw new ServletException("Failed to create database", ex);
+            conn.setAutoCommit(false);
+            PreparedStatement pstmt = conn.prepareStatement(SETUP_SQL);
+            pstmt.executeUpdate();
+            conn.commit();
+            conn.setAutoCommit(true);
+        } catch (SQLException ex) {
+            throw new ServletException("Error initializing database", ex);
         }
-    }
-
-    @Override
-    public void destroy() {
-        try {
-            if (connection != null)
-                connection.close();
-        } catch (SQLException ex) { /* TODO: log this */ }
     }
 
     @Override
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response)
-        throws ServletException, IOException {
+            throws ServletException, IOException {
+        List<String> names = new ArrayList<>();
         try {
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);
-
-            ResultSet rs = statement.executeQuery
-                ("SELECT reason, amount FROM expenses;");
+            PreparedStatement pstmt = conn.prepareStatement
+                ("SELECT id, reason, amount, date FROM expenses;");
+            ResultSet rs = pstmt.executeQuery();
             while (rs.next()) {
-                // read the result set
-                response.getWriter().write
-                    ("reason = " + rs.getString("name"));
-                response.getWriter().write(", ");
-                response.getWriter().write
-                    ("amount = " + rs.getFloat("id"));
-                response.getWriter().write("\n");
+                int        id = rs.getInt("id");
+                float  amount = rs.getFloat("amount");
+                String reason = rs.getString("reason");
+                Date   when   = rs.getDate("date");
+                names.add(reason);
             }
-        } catch (SQLException ex) {
-            response.getWriter().write(ex.getMessage());
+        } catch (SQLException e) {
+            throw new ServletException("Error reading from database", e);
         }
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/plain");
+        for (String name : names) {
+            out.println(name);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        String amount = request.getParameter("amount");
+        String reason = request.getParameter("reason");
+        try {
+            PreparedStatement pstmt = conn.prepareStatement
+            ("INSERT INTO expenses VALUES (?, ?);");
+            pstmt.setString(1, amount);
+            pstmt.setString(2, reason);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new ServletException("Error writing to database", e);
+        }
+        response.setStatus(HttpServletResponse.SC_CREATED);
     }
 }
