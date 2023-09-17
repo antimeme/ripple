44b859c00740545d69cea2556fc3b310488629e8
Jeff Gold <jgold@esclat.net>
2023-09-16 20:03:39 -0400

    Omnivore: created JavaScript module and test

--- source/omnivore.mjs
+++ source/omnivore.mjs
@@ -21,49 +21,57 @@
 // object.  Each key in the object is the name of a rule and each
 // value associated with a key is a production.
 //
-//     var language = {
-//         ws: [" ", "\t", "\r", "\n"],
-//         wsp: ["%ws", ["%ws", "%wsp"]],
-//         digit: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-//         digits: ["%digit", ["%digit", "%digits"]],
-//         number: ["%digits", ["%digits", ".", "%digits"]],
-//     };
-//
-// A production must be a string, an array or an object.
-//
-// A production string is either a literal or a rule reference.  The
-// difference is that a rule reference starts with a single '%' followed
-// by at least one other character that is not '%'.  A reference refers
-// is replaced by the result of following the rule it refers to.  A
-// literal either starts with a character that isn't '%' or starts with
-// two '%' characters (in which case these are unquoted to a single
-// one).  Literals refer directly to characters in the language.
-//
-// A production array is a series of alternatives, any one of which
-// can be used to satisfy the rule.  When an array appears as a member
-// of a production array, this represents a sequence of production
-// that must match in order.  Otherwise the array entries may be either
-// literal strings or objects.
-//
-// A production can also be an object for advanced use.  The rule for
-// an object is given by its "rule" property and its weight is given
-// by the "weight property if present.
-//
 // Here is an example grammar that parses arithmetic expressions:
 //
 //   const arithmetic = omnivore.create({
 //     "nzdigit": ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
-//     "digit": ["0", "%nzdigit"],
-//     "digits": ["", ["%digit", "%digits"]],
+//     "digit": ["0", {"weight": 9, "rule": "%nzdigit"}],
+//     "digits": [["%digit", "%digits"], ""],
 //     "number": [["%nzdigit", "%digits"]],
-//     "term": [["%number"], ["%number", " * ", "%term"]],
-//     "expression": [["%term"], ["%term", " + ", "%expression"]]
+//     "term": [["%number", " * ", "%term"], ["%number"]],
+//     "expression": [["%term", " + ", "%expression"], ["%term"]]
 //   });
 //
-// Notice that the way this grammar is defined creates no left
-// recursion and no ambiguity.  An expression like "2 * 3 + 4" can
-// only be parsed one way because factors take precedence over terms.
-// Grammars defined less carefully make create unexpected results.
+// Notice that the way this grammar is defined creates:
+//   - No left recursion (rule is never first element in production)
+//   - No ambiguity (only one parse tree possible for any value)
+//   - Greedy strategy (longer matches come first)
+// Grammars defined less carefully will create unexpected results,
+// mostly in the form of valid values not being recognized.
+//
+// Each key in the grammar is considered a rule.  The value associated
+// with a rule is called a production.  A production must be an array.
+// Each member of a production array is called an alternative and must
+// be either an array, a string or an object.
+//
+// When an element of an alternative is a string it will either be a
+// reference or a terminal.  A terminal is a literal part of a value
+// in the language described by the grammar.  A reference is replaced
+// by the corresponding rule.  References are denoted by a leading
+// percent character ('%') and another character which is something
+// else.  Two percent characters ("%%") are converted to one and
+// used as a literal instead.
+//
+// When an element of an alternative is an array it is treated as a
+// sequence of terminals and references.
+//
+// When an element of an alternative is an object it can give special
+// instructions to the generator and parser.  The object must have
+// a "rule" or "r" field, which must be a string or an array.  This
+// value is substituted for the alternative in generation and parsing.
+//
+// At the moment, the only special instruction comes from the "weight"
+// or "w" field.  This affects the probability that the alternative is
+// selected when generating strings.
+
+/**
+ * Returns true iff value represents a rule reference */
+function isReference(value) {
+    return (typeof(value) === "string") && (value.length >= 2) &&
+           (value[0] === '%') && (value[1] !== '%');
+}
+
+function getRule(value) { return value.substring(1); }
 
 function quoteRule(value) {
     return value ? value.replace(/%/g, "%%") : value;
@@ -74,26 +82,26 @@ function unquoteRule(value) {
 }
 
 /**
- * Returns true iff value represents a rule reference */
-function isReference(value) {
-    return (typeof(value) === "string") && (value.length >= 2) &&
-           (value[0] === '%') && (value[1] !== '%');
+ * Return the first numeric and non-negative argument after the first,
+ * which is returned if none of the subsequent arguments qualify. */
+function nonNegative(base, ...args) {
+    for (let ii = 0; ii < args.length; ++ii)
+        if (!isNaN(args[ii]) && (args[ii] >= 0))
+            return args[ii];
+    return base;
 }
-
-function getProductionWeight(production) {
-    return (!Array.isArray(production) &&
-            (typeof production === "object")) ?
-           production.weight : 1;
-}
-
-function getRule(value) { return value.substring(1); }
 
 /**
- * Returns a sequence of steps for a single production as an array. */
-function getProduction(production) {
-    return (Array.isArray(production) ? production :
-            ((typeof production === "object") ?
-             getProduction(production.rule) : [production]));
+ * Accepts strings or string arrays and returns an array of strings. */
+function forceStringArray(value) {
+    let result = undefined;
+    if (Array.isArray(value) &&
+        value.every(v => (typeof(v) === "string"))) {
+        result = value;
+    } else if (typeof(value) === "string") {
+        result = [value];
+    }
+    return result;
 }
 
 /**
@@ -105,10 +113,10 @@ function getProduction(production) {
  * @param current index of characters in value already consumed
  * @param value string to check for a match
  * @returns integer index after a match or negative if no match */
-function matchString(match, current, value) {
-    return ((current + match.length <= value.length) &&
-            (value.indexOf(match, current) === current)) ?
-           (current + match.length) : -1;
+function matchString(match, value, index) {
+    return ((index + match.length <= value.length) &&
+            (value.indexOf(match, index) === index)) ?
+           (index + match.length) : -1;
 }
 
 class ParseError extends Error {
@@ -117,18 +125,146 @@ class ParseError extends Error {
     }
 }
 
+/**
+ * An alternative is a single possible expansion for a production rule.
+ * Every alternative is a sequence of terminals and references to
+ * other rules.  There's no need to be concerned with weights here. */
+class Alternative {
+    constructor(rules, value) {
+        this.__elements = forceStringArray(value);
+
+        if (this.__elements) {
+            this.weight = 1;
+        } else if ((typeof(value) === "object") && value.rule) {
+            this.__elements = forceStringArray(value.rule);
+            if (this.__elements) {
+                this.weight = nonNegative(1, value.weight, value.w);
+            } else throw new TypeError(
+                "Unrecognized object rule (" + typeof(value.rule) +
+                "): " + value.rule);
+        } else if ((typeof(value) === "object") && value.r) {
+            this.__elements = forceStringArray(value.r);
+            if (this.__elements) {
+                this.weight = nonNegative(1, value.weight, value.w);
+            } else throw new TypeError(
+                "Unrecognized object r (" + typeof(value.r) +
+                "): " + value.r);
+        } else throw new TypeError(
+            "Unrecognized alternative (" + typeof(value) +
+            "): " + value);
+
+        this.__elements.forEach(element => {
+            if (isReference(element) && !(getRule(element) in rules))
+                throw new TypeError(
+                    "Missing rule for reference: " + element);
+        }, this);
+    }
+
+    static create(rules, value) {
+        return (value instanceof Alternative) ?
+               value : new Alternative(rules, value);
+    }
+
+    getWeight() { return this.weight; }
+
+    generate(rules) {
+        const result = [];
+        this.__elements.forEach(element => {
+            if (isReference(element)) {
+                const production = Production.create(
+                    rules, rules[getRule(element)]);
+                result.push(production.generate(rules));
+            } else result.push(unquoteRule(element));
+        });
+        return result.join("");
+    }
+
+    parse(rules, value, index) {
+        let current = index;
+
+        this.__elements.forEach(element => {
+            if (current < 0)
+                return;
+            if (isReference(element))
+                current = rules[getRule(element)]
+                    .parse(rules, value, current);
+            else current = matchString(
+                unquoteRule(element), value, index);
+        }, this);
+        return current;
+    }
+}
+
+class Production {
+    constructor(rules, value) {
+        if (Array.isArray(value))
+            this.__alternatives = value.map(
+                alt => Alternative.create(rules, alt));
+        else if (value && (typeof(value) === "object") &&
+                 Array.isArray(value.alts)) {
+            this.__alternatives = value.alts.map(
+                alt => Alternative.create(rules, alt));
+            // :TODO: more features here
+        } else throw new TypeError(
+            "Unrecognized production: " + typeof(value));
+    }
+
+    static create(rules, value) {
+        return (value instanceof Production) ?
+               value : new Production(rules, value);
+    }
+
+    generate(rules) {
+        const result = [];
+        let choice = Math.random();
+        let total = 0;
+        this.__alternatives.forEach(alternative =>
+            { total += alternative.getWeight(); });
+        choice *= total;
+        this.__alternatives.forEach(alternative => {
+            const weight = alternative.getWeight();
+
+            if (choice < 0) { // skip
+            } else if (choice < weight) {
+                result.push(alternative.generate(rules));
+                choice = -1;
+            } else choice -= weight;
+        });
+        return result.join("");
+    }
+
+    parse(rules, value, index) {
+        let result = undefined;
+        this.__alternatives.forEach(alt => {
+            if (isNaN(result) || (result < 0))
+                result = alt.parse(rules, value, index);
+        });
+        return result;
+    }
+}
+
+
 class Grammar {
     /**
      * Create a grammar that matches specified rules.
      *
      * @param rules an object which maps rule names to productions */
     constructor(rules) {
-        if (typeof(rules) !== "object")
-            throw new TypeError("rules must be an object");
-        else if (!Object.keys(rules).every(
-            (rule) => Array.isArray(rules[rule])))
-            throw new TypeError("all rule entries must be arrays");
-        this.__rules = rules;
+        if (!rules || (typeof(rules) !== "object"))
+            throw new TypeError("Rules must be an object");
+        this.__rules = {};
+        Object.keys(rules).forEach(rule => {
+            const production = rules[rule];
+            this.__rules[rule] =
+                (production instanceof Production) ?
+                production : new Production(rules, production);
+        }, this);
+    }
+
+    eachRule(fn, context) {
+        let index = 0;
+        Object.keys(this.__rules).forEach(rule =>
+            { fn.call(context, rule, index++, this.__rules[rule]); });
     }
 
     /**
@@ -138,66 +274,13 @@ class Grammar {
      * @param rule a string indicating the starting rule
      * @returns a string selected from the grammar. */
     generate(rule) {
-        let value = '', total = 0, choice;
-        let current = this.__rules[rule];
+        if (typeof(rule) !== "string")
+            throw new TypeError("Rule must be a string (not " +
+                                typeof(rule) + ")");
+        else if (!(rule in this.__rules))
+            throw new RangeError("Unknown rule: \"" + rule + "\"");
 
-        if (current) {
-            current.forEach(production => {
-                total += getProductionWeight(production); });
-            choice = Math.random() * total;
-            current.forEach(production => {
-                const weight = getProductionWeight(production);
-
-                if (choice < 0) { // skip
-                } else if (choice < weight) {
-                    getProduction(production)
-                        .forEach(function(component) {
-                            if (isReference(component))
-                                value += this.generate(
-                                    getRule(component));
-                            else value += unquoteRule(component);
-                        }, this);
-                    choice = -1;
-                } else choice -= weight;
-            }, this);
-        } else value = "ERROR-missing-%" + rule;
-        return value;
-    }
-
-    __parseRule(rule, start, end, value) {
-        // A recursive descent parser must either support backtracking
-        // or exclude non-deterministic grammars.  This parser does
-        // neither.  Instead it simply finds the first production
-        // rule that matches and stops there.  This means it might
-        // reject members of the language in such cases.  It's up
-        // to the caller to provide an unambigous grammar, which may
-        // be enforced by other parts of the code.
-        const productions = this.__rules[rule];
-
-        productions.forEach(production => {
-            let matched = start;
-
-            getProduction(production).forEach(element => {
-                if (matched < 0) {
-                    // Match failed on previous step
-                } else if (isReference(element)) {
-                    matched = this.__parseRule(getRule(element))
-                } else if (typeof(element) === "string") {
-                    matched = matchString(element, matched, value);
-                } else if ((typeof(element) === "object") &&
-                           isReference(element.rule)) {
-                    matched = this.__parseRule(getRule(element.rule))
-                } else if ((typeof(element) === "object") &&
-                           (typeof(element.rule) === "string")) {
-                    matched = matchString(element.rule, matched, value);
-                } else {
-                    matched = -1;
-                    throw new TypeError(
-                        "Unknown type for production " +
-                        "element: " + typeof(element));
-                }
-            }, this);
-        });
+        return this.__rules[rule].generate(this.__rules);
     }
 
     /**
@@ -206,15 +289,23 @@ class Grammar {
      * @param rule a string indicating the starting rule
      * @returns a parse tree */
     parse(rule, value) {
+        if (typeof(rule) !== "string")
+            throw new TypeError("Rule must be a string (not " +
+                                typeof(rule) + ")");
+        else if (!(rule in this.__rules))
+            throw new RangeError("Unknown rule: " + rule);
         // :TODO: detect (and correct?) left recursion
-        // :TODO: detect non-determinism
-        const matched = this.__parseRule(rule, 0, value.length, value);
-        if (matched != value.length)
-            throw new ParseError("Parsing failed: " + matched);
-    }
+        // :TODO: detect non-determinism?
 
+        const candidate = this.__rules[rule].parse(
+            this.__rules, value, 0);
+        if (candidate !== value.length)
+            throw new ParseError("Parsing failed (" + candidate + ")");
+        return candidate;
+    }
 }
 
 export default {
-    create: function(rules) { return new Grammar(rules); }
+    createGrammar: rules => new Grammar(rules),
+    ParseError
 };
