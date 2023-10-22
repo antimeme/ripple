86a98a3d967ae71bc3b2587667d4c09c9e5230e7
Jeff Gold <jgold@esclat.net>
2023-10-22 17:17:56 -0400

    camera.mjs: conditional autofill and drawBefore/drawAfter

--- source/camera.mjs
+++ source/camera.mjs
@@ -16,42 +16,53 @@
 // <http://www.gnu.org/licenses/>.
 //
 // ---------------------------------------------------------------------
-// A simple two-dimensional camera abstraction.
+// A simple two-dimensional camera abstraction for HTML canvas.
+// Camera provides a world space where the origin (0, 0) starts at
+// the center of the screen and uses chalkboard coordinates (so
+// positive x goes to the right and positive y goes up).
 
 class Camera {
     constructor(screen) {
         if (!(screen instanceof HTMLElement))
-            throw new TypeError("Camera screen must be a DOM element");
-        this.#screen = screen;
+            throw new TypeError("Camera screen must be a DOM canvas");
+        this.#screen   = screen;
         this.#position = { x: 0, y: 0 };
         this.resize();
     }
 
     #screen; // Where the drawing happens
-    #bounds; // Location of screen within client frame
-    #position; // Location of camera in world space
     #radius; // Radius of largest circle that fits in center screen
+    #bounds; // Location of screen within client frame
+
+    #position; // Location of camera in world space
     #scale = 1; // Distance of camera from world space
-    #spin = 0; // Rotation of camera in world space
-    #draw_id = 0;
-    #listeners = false;
-    #app = undefined;
+    #spin  = 0; // Rotation of camera in world space
+
+    #draw_id = 0; // Used to combine redraw requests
+    #listeners = false; // Ensures listeners get registered once
+    #app       = undefined; // Handles drawing and interaction
 
     get width() { return this.#screen.width; }
     get height() { return this.#screen.height; }
     get radius() { return this.#radius; }
 
+    /**
+     * Recalculate things that depend on the screen size */
     resize() {
         this.#bounds = this.#screen.getBoundingClientRect();
         this.#radius = Math.min(this.#screen.width,
                                 this.#screen.height) / 2;
     }
 
+    /**
+     * Convert an event location to a point relative to the screen */
     getPoint(point) {
         return {x: point.clientX - this.#bounds.x,
                 y: point.clientY - this.#bounds.y};
     }
 
+    /**
+     * Set up a context for drawing */
     configureContext(ctx) {
         ctx.clearRect(0, 0, this.#screen.width, this.#screen.height);
         ctx.save();
@@ -63,6 +74,8 @@ class Camera {
         return ctx;
     }
 
+    /**
+     * Undo context settings */
     restoreContext(ctx) { ctx.restore(); return ctx; }
 
     /**
@@ -117,8 +130,8 @@ class Camera {
     /**
      * Move the camera to the world space position provided. */
     setPosition(point) {
-        this.#position.x = point.x;
-        this.#position.y = point.y;
+        this.#position.x = -point.x;
+        this.#position.y = -point.y;
         return this;
     }
 
@@ -146,44 +159,103 @@ class Camera {
         return this.setScale(this.#scale * factor, min, max);
     }
 
+    #delegate(event) {
+        if (this.#app && (typeof(this.#app[event.type]) === "function"))
+            this.#app[event.type].call(this.#app, event, this);
+    }
+
+    /**
+     * Registers handlers for events and drawing.
+     *
+     * This is safe to call more than once to change the mode of
+     * an application. */
     manage(app) {
         this.#app = app;
 
         if (!this.#listeners) {
-            ["touchstart", "touchmove", "touchend",
-             "mousedown", "mousemove", "mouseup",
-             "click", "dblclick", "wheel"].forEach(evtype => {
-                 this.#screen.addEventListener(evtype, event => {
-                     if (typeof(this.#app[evtype]) === "function")
-                         this.#app[evtype].call(this.#app, event, this);
-                 });
-             });
+            let dragStart = undefined;
+
+            this.#screen.addEventListener("click", event => {
+                return this.#delegate(event);
+            });
+            this.#screen.addEventListener("dblclick", event => {
+                return this.#delegate(event);
+            });
+            this.#screen.addEventListener("wheel", event => {
+                if (this.#app && this.#app.autozoom) {
+                    event.preventDefault();
+                    this.redraw();
+                    this.zoom((event.wheelDeltaY > 0) ? (5/4) : (4/5),
+                              this.#app.autozoom.min,
+                              this.#app.autozoom.max);
+                }
+                return this.#delegate(event);
+            });
+            this.#screen.addEventListener("mousedown", event => {
+                if (this.#app && this.#app.autodrag)
+                    dragStart = this.toWorld(this.getPoint(event));
+                return this.#delegate(event);
+            });
+            this.#screen.addEventListener("mousemove", event => {
+                if (this.#app && this.#app.autodrag && dragStart) {
+                    const point = this.toWorld(this.getPoint(event));
+                    this.pan({x: point.x - dragStart.x,
+                              y: point.y - dragStart.y});
+                    this.redraw();
+                }
+                return this.#delegate(event);
+            });
+            this.#screen.addEventListener("mouseup", event => {
+                if (this.#app && this.#app.autodrag)
+                    dragStart = undefined;
+                return this.#delegate(event);
+            });
+            this.#screen.addEventListener("touchstart", event => {
+                return this.#delegate(event);
+            });
+            this.#screen.addEventListener("touchmove", event => {
+                return this.#delegate(event);
+            });
+            this.#screen.addEventListener("touchend", event => {
+                return this.#delegate(event);
+            });
+            this.#screen.addEventListener("touchend", event => {
+                return this.#delegate(event);
+            });
+
             window.addEventListener("resize", event => {
-                this.#screen.height =
-                    window.innerHeight || window.clientHeight;
-                this.#screen.width =
-                    window.innerWidth || window.clientWidth;
+                if (this.#app && this.#app.autofill) {
+                    this.#screen.height =
+                        window.innerHeight || window.clientHeight;
+                    this.#screen.width =
+                        window.innerWidth || window.clientWidth;
+                }
                 this.resize(screen.width, screen.height);
-                if (typeof(this.#app.resize) === "function")
-                    this.#app.resize.call(this.#app,
-                                          this.#screen.width,
-                                          this.#screen.height);
+                this.#delegate(event);
                 return this.redraw();
             });
             window.dispatchEvent(new Event("resize"));
             this.#listeners = true;
         }
-        return this;
+        return this.redraw();
     }
 
     #draw() {
+        const now = new Date().getTime();
+        if (this.#app && typeof(this.#app.update) === "function")
+            this.#app.update.call(this.#app, now, this);
+
         if (!this.#screen.getContext)
             throw Error("screen has no getContext");
         const ctx = this.#screen.getContext("2d");
+        if (this.#app && typeof(this.#app.drawBefore) === "function")
+            this.#app.drawBefore.call(this.#app, ctx, this);
         this.configureContext(ctx);
         if (this.#app && typeof(this.#app.draw) === "function")
-            this.#app.draw.call(this.#app, ctx, this)
+            this.#app.draw.call(this.#app, ctx, this);
         this.restoreContext(ctx);
+        if (this.#app && typeof(this.#app.drawAfter) === "function")
+            this.#app.drawAfter.call(this.#app, ctx, this);
         this.#draw_id = 0;
         if (this.#app &&
             ((typeof(this.#app.active) === "function") ?
@@ -195,7 +267,24 @@ class Camera {
         if (!this.#draw_id)
             this.#draw_id = requestAnimationFrame(
                 () => { this.#draw(); });
+        return this;
     }
+
+    static drawRoundRect(ctx, x, y, width, height, radius) {
+        ctx.beginPath();
+        ctx.moveTo(x + radius, y);
+        ctx.lineTo(x + width - radius, y);
+        ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+        ctx.lineTo(x + width, y + height - radius);
+        ctx.quadraticCurveTo(x + width, y + height,
+                             x + width - radius, y + height);
+        ctx.lineTo(x + radius, y + height);
+        ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+        ctx.lineTo(x, y + radius);
+        ctx.quadraticCurveTo(x, y, x + radius, y);
+        ctx.closePath();
+    }
+
 }
 
 export default Camera;
