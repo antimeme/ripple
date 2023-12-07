3f9f058ee016ab606ed7b9d215f612defad16e47
Jeff Gold <jgold@esclat.net>
2023-12-07 11:43:43 -0500

    Exploring Axum framework for Rust web server

--- source/ripple.rs
+++
@@ -1,3 +0,0 @@
-#[macro_use] extern crate rocket;
-pub mod rocketutil;
-pub mod expense;
--- source/server.rs
+++ source/server.rs
@@ -1,30 +1,30 @@
-// :TODO: request optional TLS client certificates
-use rocket::fs::relative;
-use ripple::rocketutil::FileExtServer;
-use ripple::rocketutil::bootstrap_tls;
+use std::net::SocketAddr;
+use chrono::{prelude::Utc, SecondsFormat};
+use axum;
 
-fn create_server() -> rocket::Rocket<rocket::Build> {
-    use figment::providers::{Format, Json};
-
-    let figment = rocket::Config::figment()
-        .merge(Json::file("/etc/ripple/server.json"))
-        .merge(Json::file("./server.json"))
-        .join(("address", "0.0.0.0"))
-        .join(("port", 7878))
-        .join(("tls.key", "./server-key.pem"))
-        .join(("tls.certs", "./server-chain.pem"))
-        .join(("databases.expensedb.url", "./expense.db"));
-    bootstrap_tls(&figment);
-
-    let server = rocket::custom(figment);
-    ripple::expense::expense_server(server)
-        .mount("/", FileExtServer::new(relative!("index.html")))
-        .mount("/favicon.ico", FileExtServer::new(
-            relative!("resources/images/ripple.png")).rank(1))
-        .mount("/apps", FileExtServer::new(relative!("apps")))
-        .mount("/slides", FileExtServer::new(relative!("slides")))
+fn log(message: &str) {
+    println!("{} {message}", Utc::now().to_rfc3339_opts
+             (SecondsFormat::Millis, true));
 }
 
-#[rocket::main]
-async fn main() -> Result<(), rocket::Error>
-{ let _rocket = create_server().launch().await?; Ok(()) }
+async fn logging(req: axum::http::Request<axum::body::Body>,
+                 next: axum::middleware::Next<axum::body::Body>) ->
+    axum::response::Response
+{
+    log(&format!("URI: {}", req.uri()));
+    next.run(req).await
+}
+
+#[tokio::main]
+async fn main() {
+    let port = 7878;
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    let app = axum::Router::new()
+        .route("/", axum::routing::get(|| async { "Hello, Rust!" }))
+        .layer(axum::middleware::from_fn(logging));
+
+    log(&format!("starting server on http://localhost:{port}"));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await.unwrap();
+}
