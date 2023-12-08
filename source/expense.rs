fe150f0464fe264addd178c90149b82a645ad49a
Jeff Gold <jgold@esclat.net>
2023-12-07 21:47:36 -0500

    server.rs: reworked expense to use Axum

--- source/expense.rs
+++ source/expense.rs
@@ -1,51 +1,9 @@
-// :TODO: store tag values
-// :TODO: support deleting expense records
-// :TODO: calculate tag totals
-// :TODO: support filtering by month
-use rocket::fs::relative;
-use rocket::serde::{Serialize, Deserialize, json::Json};
-use rocket_db_pools::{self, sqlx::{self, Row}, Database, Connection};
-use crate::rocketutil::FileExtServer;
-
-#[derive(rocket_db_pools::Database)]
-#[database("expensedb")]
-struct ExpenseDB(sqlx::SqlitePool);
-
-async fn expense_db_init(
-    rocket: rocket::Rocket<rocket::Build>
-) -> rocket::fairing::Result {
-    match ExpenseDB::fetch(&rocket) {
-        Some(pool) => {
-            match pool.acquire().await {
-                Ok(mut db) =>
-                    match sqlx::query(r#"
-                CREATE TABLE IF NOT EXISTS expenses (
-                    id INTEGER PRIMARY KEY,
-                    reason TEXT NOT NULL,
-                    amount REAL NOT NULL,
-                    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                );"#).execute(&mut db).await {
-                    Ok(_) => Ok(rocket),
-                    Err(err) => {
-                        error!("Failed to initialize SQL database: {}",
-                               err.to_string());
-                        Err(rocket)
-                    }
-                },
-                Err(err) => {
-                    error!("Failed to get SQL connection: {}",
-                           err.to_string());
-                    Err(rocket)
-                }
-            }
-        },
-        None => Err(rocket)
-    }
-}
+use axum;
+use sqlx::{self, Row};
+use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize)]
-#[serde(crate = "rocket::serde")]
-struct Expense {
+pub struct Expense {
     id:     i32,
     amount: f32,
     reason: String,
@@ -54,22 +12,19 @@ struct Expense {
 }
 
 #[derive(Serialize, Deserialize)]
-#[serde(crate = "rocket::serde")]
-struct ExpenseList {
+pub struct ExpenseList {
     expenses: Vec<Expense>
 }
 
-#[get("/list", format="json")]
-async fn expense_list(
-    mut db: Connection<ExpenseDB>
-) -> Result<Json<ExpenseList>, String>
+async fn list(axum::extract::Extension(pool):
+                  axum::extract::Extension<sqlx::SqlitePool>
+) -> Result<axum::Json<ExpenseList>, String>
 {
     let mut list = ExpenseList { expenses: Vec::new() };
-    match sqlx::query(concat!(
-        "SELECT id, amount, reason, date ",
-        "  FROM expenses",
-        " ORDER BY date;"))
-        .fetch_all(&mut *db).await {
+    match sqlx::query(r#"
+        SELECT id, amount, reason, date
+          FROM expenses ORDER BY date;"#)
+        .fetch_all(&pool).await {
             Ok(cursor) => {
                 for row in cursor {
                     list.expenses.push(Expense {
@@ -80,39 +35,47 @@ async fn expense_list(
                         tags:   Vec::new()
                     });
                 }
-                Ok(Json(list))
+                Ok(axum::Json(list))
             },
             Err(e) => { Err(e.to_string()) }
         }
 }
 
-#[post("/add", format="json", data="<expense>")]
-async fn expense_add(
-    mut db: Connection<ExpenseDB>,
-    expense: Json<Expense>
-) -> String
+async fn add(axum::extract::Extension(pool):
+                 axum::extract::Extension<sqlx::SqlitePool>,
+                 expense: axum::Json<Expense>) -> String
 {
-    println!("Date: {}", &expense.date);
-    match sqlx::query(concat!(
-        "INSERT INTO expenses (reason, amount, date) ",
-        "VALUES ($1, $2, $3);"))
+    match sqlx::query(r#"
+        INSERT INTO expenses (reason, amount, date)
+        VALUES ($1, $2, $3);"#)
         .bind(&expense.reason)
         .bind(expense.amount)
         .bind(&expense.date)
-        .execute(&mut *db).await {
+        .execute(&pool).await {
             Ok(_) => "Success".to_string(),
             Err(err) => err.to_string()
         }
 }
 
-pub fn expense_server(
-    server: rocket::Rocket<rocket::Build>
-) -> rocket::Rocket<rocket::Build> {
-    server
-        .attach(ExpenseDB::init())
-        .attach(rocket::fairing::AdHoc::try_on_ignite(
-            "Expense SQL Initialization", expense_db_init))
-        .mount("/expense", FileExtServer::new(
-            relative!("apps/expense.html")))
-        .mount("/expense", routes![expense_add, expense_list])
+
+pub async fn setup(app: axum::Router) -> axum::Router {
+    let pool = sqlx::Pool::<sqlx::Sqlite>::connect_with(
+        sqlx::sqlite::SqliteConnectOptions::new()
+            .create_if_missing(true)
+            .journal_mode(sqlx::sqlite::SqliteJournalMode::Wal)
+            .synchronous(sqlx::sqlite::SqliteSynchronous::Normal)
+            .filename(&"expense.db"), )
+        .await.expect("Failed to create pool");
+    sqlx::query(r#"
+        CREATE TABLE IF NOT EXISTS expenses (
+               id INTEGER PRIMARY KEY,
+               reason TEXT NOT NULL,
+               amount REAL NOT NULL,
+               date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);"#, )
+        .execute(&pool)
+        .await.expect("Failed to create table");
+    app
+        .route("/expense/list", axum::routing::get(list))
+        .route("/expense/add", axum::routing::post(add))
+        .layer(axum::extract::Extension(pool))
 }
--- source/server.rs
+++ source/server.rs
@@ -1,30 +1,40 @@
 use std::net::SocketAddr;
-use chrono::{prelude::Utc, SecondsFormat};
+use std::path::PathBuf;
+use chrono;
+use axum_server;
 use axum;
 
+mod expense;
+
 fn log(message: &str) {
-    println!("[{}] {message}", Utc::now().to_rfc3339_opts
-             (SecondsFormat::Millis, true));
+    println!("[{}] {message}",
+             chrono::prelude::Utc::now().to_rfc3339_opts
+             (chrono::SecondsFormat::Millis, true));
 }
 
-async fn logging(req: axum::http::Request<axum::body::Body>,
+async fn logging(request: axum::http::Request<axum::body::Body>,
                  next: axum::middleware::Next<axum::body::Body>) ->
     axum::response::Response
 {
-    log(&format!("URI: {}", req.uri()));
-    next.run(req).await
+    log(&format!("{} {}", request.method(), request.uri().path()));
+    next.run(request).await
 }
 
 #[tokio::main]
 async fn main() {
-    let port = 7878;
-    let addr = SocketAddr::from(([0, 0, 0, 0], port));
-    let app = axum::Router::new()
+    let port = 7878; // TODO: make configurable
+    let app = expense::setup(axum::Router::new()).await
         .route("/", axum::routing::get(|| async { "Hello, Rust!" }))
         .layer(axum::middleware::from_fn(logging));
 
     log(&format!("START http://localhost:{port}"));
-    axum::Server::bind(&addr)
+    axum_server::bind_rustls(
+        SocketAddr::from(([0, 0, 0, 0], port)),
+        axum_server::tls_rustls::RustlsConfig::from_pem_file(
+            PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+                .join("server-cert.pem"),
+            PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+                .join("server-key.pem")).await.unwrap())
         .serve(app.into_make_service())
         .await.unwrap();
 }
